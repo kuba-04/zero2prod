538aea3038561506070e76864a909637cdbcfa39
kuba <jakub.gladysz1@gmail.com>
2024-03-28 16:45:42 +0100

    add telemetry

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,13 +1,35 @@
 use actix_web::{HttpResponse, web};
-use sqlx::{PgPool};
-use sqlx::types::{chrono, uuid};
-use uuid::Uuid;
 use chrono::Utc;
+use sqlx::PgPool;
+use sqlx::types::{chrono, uuid};
+use tracing_futures::Instrument;
+use uuid::Uuid;
 
 pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );
+    request_span.enter();
+    let query_span = tracing::info_span!(
+    "Saving new subscriber details in the database"
+    );
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -18,12 +40,23 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(db_pool.as_ref())
-    .await
+        .execute(db_pool.as_ref())
+        .instrument(query_span)
+        .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
@@ -32,5 +65,5 @@ pub async fn subscribe(
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
-    name: String
+    name: String,
 }
--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
 use sqlx::{Pool, Postgres};
+use actix_web::middleware::Logger;
 
 use crate::routes::{health_check, subscribe};
 
@@ -13,6 +14,7 @@ pub fn run(
     let connection = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
