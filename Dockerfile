e3bbf38492e591b2360188d4fa496ad7e6bce02d
kuba <jakub.gladysz1@gmail.com>
2024-04-06 10:56:48 +0200

    optimize docker image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,21 @@
-FROM rust:1.76.0
+# Builder stage
+FROM rust:1.76.0 AS builder
+
 WORKDIR /app
 COPY . .
 ENV SQLX_OFFLINE true
 RUN cargo build --release
+
+# Runtime stage
+FROM debian:bullseye-slim AS runtime
+WORKDIR /app
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends openssl \
+    # Clean up
+    && apt-get autoremove -y \
+    && apt-get clean -y \
+    && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+COPY configuration configuration
 ENV APP_ENVIRONMENT production
-ENTRYPOINT ["./target/release/zero2prod"]
+ENTRYPOINT ["./zero2prod"]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,7 +13,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
+        .await
         .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
