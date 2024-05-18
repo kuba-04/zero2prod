459089272228ee1e968b4c4a0ea73ee1e21060ef
kuba <jakub.gladysz1@gmail.com>
2024-05-18 15:49:14 +0200

    fix db data 2

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -53,7 +53,7 @@ async fn insert_subscriber(
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, &5)
+        VALUES ($1, $2, $3, $4, $5)
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
