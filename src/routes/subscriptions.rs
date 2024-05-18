ffd6ed870e0242e6dd1fa218402f86457f7ede04
kuba <jakub.gladysz1@gmail.com>
2024-05-18 15:56:34 +0200

    fix db data 3

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -52,14 +52,13 @@ async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, $5)
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
-        Utc::now(),
-        "confirmed"
+        Utc::now()
     )
         .execute(pool)
         .await
